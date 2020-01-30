d56fa1ce4da3829902039eef4c5ab83a5e4a3c8d
Randee Orion <randeegood@gmail.com>
2020-01-29 20:47:04 -0800

    feature one is complete!

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ app.get('/', (request, response) => {
 app.get('/location', locationHandler);
 app.get('/weather', weatherHandler);
 //app.get('/eventful', eventfulHandler)
+app.get('/movies', moviesHandler);
 
 //Location Handler Function
 function locationHandler(request, response){
@@ -92,9 +93,42 @@ function weatherHandler(request, response) {
     });
 }
 
+
+
+function moviesHandler(request, response){
+  console.log('inside movie handler');
+  let city = request.query.search_query;
+  console.log(city);
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&language=en-US&query=${city}&page=1&include_adult=false`;
+
+  superagent.get(url)
+    .then(data => {
+      let movieData = data.body.results.map(city =>{
+        return new Movie(city);
+      });
+      console.log('movie data = ', movieData);
+      response.status(200).json(movieData);
+    })
+    .catch(() => {
+      errorHandler('So sorry, the dwarves mined too deep and found the balrog.', request, response);
+    });
+}
+
+
+
 function Weather(day){
   this.forecast = day.summary;
   this.time = new Date(day.time * 1000).toString().slice(0,15);
+}
+
+function Movie(city){
+  this.title = city.title,
+  this.overview = city.overview,
+  this.average_votes = city.vote_average,
+  this.total_votes = city.vote_count,
+  this.image_url = `https://image.tmdb.org/t/p/w500${city.poster_path}`,
+  this.popularity = city.popularity,
+  this.released_on = city.release_date;
 }
 
 
